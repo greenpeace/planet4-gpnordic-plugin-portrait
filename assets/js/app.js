fbb003ea118638d7a1995b8e4ae298899737f299
Jens Lernstål Waern <jens@simmalugnt.se>
2020-05-15 13:39:24 +0200

    Hiding downloadbutton if caption is empty

--- assets/js/app.js
+++ assets/js/app.js
@@ -146,7 +146,7 @@ jQuery(function() {
 					<img :src="composition" />
 					<div class="caption tp-30 bp-50">
 						<div v-html="thank_you_text"></div>
-						<div class="tm-30">
+						<div class="tm-30" v-show="greenpeace_petition_ajax.translations['Download image'] !== ''">
 							<a :href="thank_you_image" target="_blank" class="button" v-html="greenpeace_petition_ajax.translations['Download image']"></a>
 						</div>
 						<div class="tm-30">
